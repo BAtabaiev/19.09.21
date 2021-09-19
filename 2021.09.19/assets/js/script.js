67d0aef2e337ff4f85cf843d3bc6437a3a30bded
BAtabaiev <87579276+BAtabaiev@users.noreply.github.com>
2021-09-19 13:50:32 +0300

    Add files via upload

---
+++ 2021.09.19/assets/js/script.js
@@ -0,0 +1,33 @@
+'use strict';
+//=================НЕДОЗМЕЙКА==================
+/*let board = document.querySelector('#board');
+const colors = ['#FF4500', '#BDB76B', '#DC143C', '#FAFAD2', '#A0522D', '#9400D3', '#40E0D0', '#00FA9A']; 
+const NUMBER = 812;
+
+for(let i = 0; i < NUMBER; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square) )
+    square.addEventListener('mouseleave', () => removeColor(square))
+   
+
+    board.append(square)
+}
+
+function setColor(el){
+    const color = getRandomColor()
+    el.style.backgroundColor = color
+    el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(el){
+    el.style.backgroundColor = 'black'
+    el.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor(el){
+    const random = Math.floor(Math.random()*colors.length)
+    return colors[random];
+}
+*/
